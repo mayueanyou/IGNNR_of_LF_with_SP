6b921bb60f6452c0720c4fd2b30e837c311a9abe
yuema <mayueanyou@gmail.con>
2022-05-14 18:19:18 -0400

    update

--- figure.py
+++ figure.py
@@ -41,7 +41,44 @@ def plot_2(path,name,acc_list,loss_list,gradient_list):
     plt.savefig(path)
     #plt.show()
 
-def read_file(name,path,name2):
+def plot_3(path,name,acc_list,loss_list,gradient_list):
+    index = np.arange(1,6,1)
+    for i in range(len(loss_list)):
+        loss_list[i] = np.sum(np.array(loss_list[i]),axis=1)/len(loss_list[i][0])
+        gradient_list[i] = np.sum(np.array(gradient_list[i]),axis=1)/len(gradient_list[i][0])
+    loss = loss_list
+    gradient = gradient_list
+    fig, ax = plt.subplots(3,1)
+
+    ax[0].set_title('val acc')
+    ax[1].set_title('average loss')
+    ax[2].set_title('average gradient')
+    ax[0].set_xticks(np.arange(1,6,1))
+    ax[1].set_xticks(np.arange(1,6,1))
+    ax[2].set_xticks(np.arange(1,6,1))
+    ax[2].set_xlabel('epoch')
+
+    for i in range(3):
+        if i ==0:
+            color='r'
+        elif i==1:
+            color='b'
+        elif i==2:
+            color='g'
+        ax[0].plot(index,np.array(acc_list[i]),c=color,label='seed %d'%(i+1))
+        ax[1].plot(index,loss[i],c=color,label='seed %d'%(i+1))
+        ax[2].plot(index,gradient[i],c=color,label='seed %d'%(i+1))
+
+    ax[0].legend(loc="upper left")
+    ax[1].legend(loc="lower left")
+    ax[2].legend(loc="lower left")
+    fig.tight_layout()
+    plt.suptitle(name,x=0.1)
+    fig.set_size_inches(5, 7)
+    plt.savefig(path)
+    #plt.show()
+
+def read_file(name):
     def tf_data(data):
         for i in range(len(data)):
             data[i].replace('\n','')
@@ -92,14 +129,27 @@ def read_file(name,path,name2):
         train_gradient_list.append(tf_data(train_gradient))
     val_acc = tf_data(val_acc)
     val_f1 = tf_data(val_f1)
+    return val_acc,train_loss_list,train_gradient_list
     #plot(train_acc_list,train_loss_list,train_gradient_list)
-    plot_2(path,name2,val_acc,train_loss_list,train_gradient_list)
+    #plot_2(path,name2,val_acc,train_loss_list,train_gradient_list)
 
 def plot_experiments():
     files=os.listdir('./experiments')
     for i in files:
-        read_file('./experiments/%s/log.txt'%i,'./experiments/%s/%s.png'%(i,i),i)
+        val_acc,train_loss_list,train_gradient_list = read_file('./experiments/%s/log.txt'%i,)
+        plot_2('./experiments/%s/%s.png'%(i,i),i,val_acc,train_loss_list,train_gradient_list)
 
+def plot_experiments_2():
+    files=os.listdir('./experiments')
+    files.sort()
+    for i in range(4):
+        val_acc1,train_loss_list1,train_gradient_list1=read_file('./experiments/'+files[0+i]+'/log.txt')
+        val_acc2,train_loss_list2,train_gradient_list2=read_file('./experiments/'+files[4+i]+'/log.txt')
+        val_acc3,train_loss_list3,train_gradient_list3=read_file('./experiments/'+files[8+i]+'/log.txt')
+        va_list,tl_list,tg_list = [val_acc1,val_acc2,val_acc3],[train_loss_list1,train_loss_list2,train_loss_list3],[train_gradient_list1,train_gradient_list2,train_gradient_list3]
+        name = ['0_16','0_32','3_16','3_32']
+        plot_3('./%d'%i,name[i],va_list,tl_list,tg_list)
 if __name__ == '__main__':
+    plot_experiments_2()
     #plot_experiments()
-    read_file('/log.txt','./log.png','log')
+    #read_file('/log.txt','./log.png','log')
